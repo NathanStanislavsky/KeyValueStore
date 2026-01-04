1506c9b5d9ee6a7384b326c433876badf411168d
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2026-01-04 14:34:15 -0500

    update CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <cassert>
+#include "wal.h"
+#include "memtable.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    
+
+    return 0;
 }
