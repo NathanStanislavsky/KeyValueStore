1e66d7e0deb76b4a4ce79f5122d24f42e7866ab3
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2026-01-05 17:25:01 -0500

    fixed cmake typo

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
-#include <cassert>
-#include "wal.h"
-#include "memtable.h"
 #include "kvstore.h"
 
-using namespace std;
-
 int main() {
-    KVStore store("test.log");
+    KVStore store("data.log");
 
-    std::cout << "putting data..." << std::endl;
-    store.put("user:1", "Nathan");
-    store.put("user:2", "Emma");
-
-    std::cout << "getting data..." << std::endl;
-    auto result = store.get("user:1");
-
-    if (result) {
-        std:cout << "Found: " << *result << std::endl;
-    }
+    std::cout << "Inserting 3 keys (Trigger limit is 3)..." << std::endl;
+    store.put("A", "1");
+    store.put("B", "2");
+    store.put("C", "3"); 
 
     return 0;
 }
