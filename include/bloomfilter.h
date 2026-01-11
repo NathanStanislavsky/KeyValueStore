56399fd0c7a240bbf6631587a354d39ab56ff969
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2026-01-11 15:57:07 -0500

    bloom filters

---
+++ include/bloomfilter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+
+
+class BloomFilter {
+    public:
+        BloomFilter(size_t numKeys, int k = 3);
+
+        void add(const std::string& key);
+
+        bool contains(const std::string& key) const;
+    private:
+        std::vector<bool> bits;
+
+        std::vector<size_t> getHashIndices(const std::string& key) const;
+
+        int k;
+};
---
+++ src/bloomfilter.cpp
@@ -0,0 +1,45 @@
+#include "bloomfilter.h"
+#include <functional>
+
+BloomFilter::BloomFilter(size_t numKeys, int k) : k(k) {
+
+    if (numKeys <= 0) {
+        throw std::invalid_argument("numKeys must be greater than 0");
+    }
+
+    bits.resize(numKeys * 10);
+}
+
+std::vector<size_t> BloomFilter::getHashIndices(const std::string& key) const {
+    std::vector<size_t> indices;
+
+    size_t hash1 = std::hash<std::string>()(key);
+    size_t hash2 = hash1 >> 16;
+
+    for (int i = 0; i < k; i++) {
+        size_t index = (hash1 + i * hash2) % bits.size();
+        indices.push_back(index);
+    }
+
+    return indices;
+}
+
+void BloomFilter::add(const std::string& key) {
+    std::vector<size_t> indices = getHashIndices(key);
+
+    for (size_t index : indices) {
+        bits[index] = true;
+    }
+}
+
+bool BloomFilter::contains(const std::string& key) const {
+    std::vector<size_t> indices = getHashIndices(key);
+
+    for (size_t index : indices) {
+        if (!bits[index]) {
+            return false;
+        }
+    }
+
+    return true;
+}
